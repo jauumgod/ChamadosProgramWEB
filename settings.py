6f708f1e8d4339064c1fbb6db174a70525c9816d
Joao Marcos D M <69704112+jauumgod@users.noreply.github.com>
2022-10-13 19:53:53 -0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,13 @@
+import __init__ as init
+
+@app.route("/")
+def delete_user():
+  return pass
+
+@app.route("/")
+def choose_user():
+  return pass
+
+@app.route("/")
+def add_user():
+  return pass
