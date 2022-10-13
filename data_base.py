5c6d650d26aaad26c799ad08da0b1d68113ae032
Joao Marcos D M <69704112+jauumgod@users.noreply.github.com>
2022-10-13 19:48:28 -0300

    Update data_base.py

--- data_base.py
+++ data_base.py
@@ -12,5 +12,17 @@ class User(db.Model):
     def __str__(self):
       return self.name
     
+  class chamados(db.Model):
+    __tablename__ = 'chamados'
+    chamados_add = usuarios()
+    chamados_add.id = db.column(db.Integer, primary_key=True)
+    chamados_add.to = db.column(db.String(84), nullable=False)
+    chamados_add.conteudo = db.column(db.String(84), nullable=False)
+    chamados_add.from = db.column(db.String(84), nullable=False)
+    chamados_add.date = db.column(db.date_time(84), nullable=False)
+
     
+    def __str__(self):
+      return self.name
+      
       
