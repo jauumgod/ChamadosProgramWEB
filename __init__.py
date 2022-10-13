496d9a370541598ac619ab9289ecf87b45502730
Joao Marcos D M <69704112+jauumgod@users.noreply.github.com>
2022-10-13 19:45:14 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 #app flask
 from flask import Flask, url_for, render_template, redirect
+import data_base as db
 
 app = Flask (__name__)
 app.config['SECRET_KEY]= 'UZUMYMW_15032325'
@@ -9,10 +10,21 @@ app.config['SECRET_KEY]= 'UZUMYMW_15032325'
 def inicio():
   return render_template("inicio.html")
 
+@app.route("/register")
+def register():
+  return render_template("register.html")
+
+@app.route("/login")
+def login():
+  return render_template("login.html")
 
 @app.route("/chamados")
 def chamados():
   return render_template("chamados.html")
+           
+@app.route("/settings")
+def settings():
+  return render_template("settings.html")
 
 if __name__ =="__main__":
   app.run(debug=True)
